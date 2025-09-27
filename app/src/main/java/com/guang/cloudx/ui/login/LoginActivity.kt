f84abd262db4bdec1a05138f7cc58b11fb34e6a4
dartcv <2968447202@qq.com>
2025-09-27 22:57:32 +0800

    feat: add navigation button back behaviour to login activity

--- app/src/main/java/com/guang/cloudx/ui/login/LoginActivity.kt
+++ app/src/main/java/com/guang/cloudx/ui/login/LoginActivity.kt
@@ -1,20 +1,24 @@
 package com.guang.cloudx.ui.login
 
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.button.MaterialButton
 import com.guang.cloudx.BaseActivity
 import com.guang.cloudx.R
 import com.guang.cloudx.logic.utils.showSnackBar
 import kotlin.getValue
 
-class LoginActivity : BaseActivity() {
-    private val textInput by lazy { findViewById<EditText>(R.id.cookieEditText)}
+class LoginActivity : BaseActivity(), View.OnClickListener {
+    private val textInput by lazy { findViewById<EditText>(R.id.cookieEditText) }
     private val button by lazy { findViewById<MaterialButton>(R.id.saveButton) }
+
+    private val topAppBar by lazy { findViewById<MaterialToolbar>(R.id.topAppBar) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,9 +31,19 @@ class LoginActivity : BaseActivity() {
         }
 
         textInput.setText(prefs.getCookie())
-        button.setOnClickListener {
-            prefs.putCookie(textInput.text.toString())
-            button.showSnackBar("已保存")
+
+        topAppBar.setNavigationOnClickListener(this)
+        button.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.saveButton -> {
+                prefs.putCookie(textInput.text.toString())
+                button.showSnackBar("已保存")
+            }
+            R.id.topAppBar -> finish()
         }
     }
+
 }
